fb9683b71f32d187b8addfa9fc598586a5367c78
KhomsiAdam <khomsiadam@gmail.com>
2021-03-05 17:24:38 +0100

    Added dashboard html page and css for mobile and some folders (icons, images, css, javascript)

---
+++ javascript/dashboard.js
@@ -0,0 +1,5 @@
+function addProduct() {
+    document.querySelector("body > main > div.add-product").style.display = "flex";
+    document.querySelector("body > footer > div > button.confirm").style.display = "block";
+    document.querySelector("body > footer > div > button.add").style.display = "none";
+}
