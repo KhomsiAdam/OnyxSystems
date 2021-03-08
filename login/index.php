33d4d31de965e9363f292677387964f17ff22aba
KhomsiAdam <khomsiadam@gmail.com>
2021-03-08 10:38:13 +0100

    fixed login page shrinking issue

--- login/index.php
+++ login/index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Onyx Systems</title>
-	<link rel="stylesheet" href="Login-style.css">
-</head>
-<body>
-     <form action="login.php" method="post" id="form">
-     	<div class="Title">	
-	 <h2>Onyx Systems</h2>
-	 	</div>
-     	
-			<label>
-				<div class="input-username-div">
-     	<input type="text" name="uname" placeholder="Username" id="username" autocomplete="off"><br>
-		 </div>
-				<div class="input-password-div">
-     	<input type="password" name="password" placeholder="Password" id="password"><br>
-		 		</div>
-			 </label>
-			 <div class="error-div">
-			 <?php if (isset($_GET['error'])) { ?>
-     		<p class="error"><?php echo $_GET['error']; ?></p>
-     	<?php } ?>
-		 </div>
-			 <div class="button-tag">
-     	<button type="submit" onclick="checkempty()">LOG IN</button>
-		
-		 	</div>
 
-     </form>
-	 <script src="script.js"></script>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="index.css">
+	<title>Onyx Systems</title>
+</head>
+
+<body>
+		<form action="login.php" method="post" id="form">
+			<h2 class="title">Onyx Systems</h2>
+			<div class="input-username-div">
+				<input type="text" name="uname" placeholder="Username" id="username" autocomplete="off"><br>
+			</div>
+			<div class="input-password-div">
+				<input type="password" name="password" placeholder="Password" id="password"><br>
+			</div>
+			</label>
+			<div class="error-div">
+				<?php if (isset($_GET['error'])) { ?>
+					<p class="error"><?php echo $_GET['error']; ?></p>
+				<?php } ?>
+			</div>
+			<div class="button-tag">
+				<button type="submit" onclick="checkempty()">LOG IN</button>
+			</div>
+		</form>
+	<script src="script.js"></script>
 </body>
+
 </html>
